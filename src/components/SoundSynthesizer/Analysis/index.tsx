f318a4dea0c856a63ababd5417122a75fbbdb66e
Raind <127611616+Juemuren@users.noreply.github.com>
2026-06-07 23:44:26 +0800

    docs: 分析子模块补上 i18n

--- src/components/SoundSynthesizer/Analysis/index.tsx
+++ src/components/SoundSynthesizer/Analysis/index.tsx
@@ -1,20 +1,21 @@
+import { useTranslation } from 'react-i18next';
 import useAudioAnalysis from '../../../hooks/synth/useAudioAnalysis';
 
 function Analysis() {
+  const { t } = useTranslation('synth');
   const { frequencyCanvasRef, timeDomainCanvasRef } = useAudioAnalysis();
 
   return (
     <div className="grid gap-4 lg:grid-cols-2">
       <div className="space-y-2">
-        {/* TODO i18n */}
-        <h3 className="font-bold">频域</h3>
+        <h3 className="font-bold">{t('analysis.frequencyDomain')}</h3>
         <canvas
           ref={frequencyCanvasRef}
           className="block w-full h-36 rounded-lg border border-app-border dark:border-app-border-dark"
         />
       </div>
       <div className="space-y-2">
-        <h3 className="font-bold">时域</h3>
+        <h3 className="font-bold">{t('analysis.timeDomain')}</h3>
         <canvas
           ref={timeDomainCanvasRef}
           className="block w-full h-36 rounded-lg border border-app-border dark:border-app-border-dark"
