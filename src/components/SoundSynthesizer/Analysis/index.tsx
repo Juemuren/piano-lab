5bd21af7ee3372286347a01c448ae6727e883e61
Raind <127611616+Juemuren@users.noreply.github.com>
2026-06-08 17:16:34 +0800

    style: 改变分析模块中的布局

--- src/components/SoundSynthesizer/Analysis/index.tsx
+++ src/components/SoundSynthesizer/Analysis/index.tsx
@@ -6,21 +6,21 @@ function Analysis() {
   const { frequencyCanvasRef, timeDomainCanvasRef } = useAudioAnalysis();
 
   return (
-    <div className="grid gap-4 lg:grid-cols-2">
-      <div className="space-y-2">
-        <h3 className="font-bold">{t('analysis.frequencyDomain')}</h3>
+    <div className="space-y-4">
+      <details open className="space-y-2">
+        <summary className="font-bold">{t('analysis.frequencyDomain')}</summary>
         <canvas
           ref={frequencyCanvasRef}
           className="block w-full h-36 rounded-lg border border-app-border dark:border-app-border-dark"
         />
-      </div>
-      <div className="space-y-2">
-        <h3 className="font-bold">{t('analysis.timeDomain')}</h3>
+      </details>
+      <details open className="space-y-2">
+        <summary className="font-bold">{t('analysis.timeDomain')}</summary>
         <canvas
           ref={timeDomainCanvasRef}
           className="block w-full h-36 rounded-lg border border-app-border dark:border-app-border-dark"
         />
-      </div>
+      </details>
     </div>
   );
 }
