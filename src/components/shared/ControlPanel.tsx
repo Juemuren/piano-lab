1b29f4d2254340f92edc396458a9737374482d0e
Raind <127611616+Juemuren@users.noreply.github.com>
2026-08-02 18:53:20 +0800

    style: 去除 ControlPanel 的阴影效果

--- src/components/shared/ControlPanel.tsx
+++ src/components/shared/ControlPanel.tsx
@@ -8,7 +8,7 @@ interface ControlPanelProps {
 function ControlPanel({ children, className = '' }: ControlPanelProps) {
   return (
     <div
-      className={`w-full rounded-3xl border border-app-border bg-app-mantle/50 p-5 shadow-app-overlay/5 shadow-xl dark:border-app-border-dark dark:bg-app-mantle-dark/50 ${className}`}
+      className={`w-full rounded-3xl border border-app-border bg-app-mantle/50 p-5 dark:border-app-border-dark dark:bg-app-mantle-dark/50 ${className}`}
     >
       {children}
     </div>
